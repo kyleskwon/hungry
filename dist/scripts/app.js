b96a7fd7d947b6225913ee1534dd1f25e6b53422
Kyle Kwon <kyleskwon@gmail.com>
2016-09-03 08:06:31 -0400

    repeat meals in master meal feed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,42 +1,37 @@
-//var app = angular.module('hungry', ['ui.router']);
-//
-//app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
-//    $locationProvider
-//        .html5Mode({
-//            enabled: true,
-//            requireBase: false
-//        });
-//    
-//    $urlRouterProvider.otherwise("/master");
-//    
-//    $stateProvider
-//        .state('master', {
-//            url: '/master',
-//            controller: 'MasterCtrl',
-//            templateUrl: '/templates/master.html'
-//        })
-//        .state('detail', {
-//            url: '/detail',
-//            controller: 'DetailCtrl',
-//            templateUrl: '/templates/detail.html'
-//        });
-//}]);
-//
-//app.controller("MasterCtrl", function($scope, $http) {
-//    $http.get("https://api.tryhungry.com/api/v1/search.json?latitude=38.896127&longitude=-77.0735961")
-//      .then(function(response) {
-//          $scope.meal = response.data;
-//      });
-//});
-//
-//app.controller("DetailCtrl", function($scope, $http) {
-//    $http.get("https://api.tryhungry.com/api/v1/search.json?latitude=38.896127&longitude=-77.0735961")
-//      .then(function(response) {
-//          $scope.meal = response.data;
-//      });
-//});
+var app = angular.module('hungry', ['ui.router']);
 
-angular.module('hungry', [
-  'hungry.controllers',
-  'hungry.services'
-]);
+app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
+    $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    
+    $urlRouterProvider.otherwise("/master");
+    
+    $stateProvider
+        .state('master', {
+            url: '/master',
+            controller: 'MasterCtrl',
+            templateUrl: '/templates/master.html'
+        })
+        .state('detail', {
+            url: '/detail',
+            controller: 'DetailCtrl',
+            templateUrl: '/templates/detail.html'
+        });
+}]);
+
+app.controller("MasterCtrl", function($scope, $http) {
+    $http.get("https://api.tryhungry.com/api/v1/search.json?latitude=38.896127&longitude=-77.0735961")
+      .then(function(response) {
+          $scope.meal = response.data;
+      });
+});
+
+app.controller("DetailCtrl", function($scope, $http) {
+    $http.get("https://api.tryhungry.com/api/v1/search.json?latitude=38.896127&longitude=-77.0735961")
+      .then(function(response) {
+          $scope.meal = response.data;
+      });
+});
